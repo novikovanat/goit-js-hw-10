fbd865ef7a6c5630a89ef88636904e5ef8473d3f
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-03-05 20:15:21 +0100

    promise made

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,80 @@
+
+import iziToast from "izitoast";
+
+const form = document.querySelector('.form')
+const buttonSubmit = document.querySelector('button')
+
+form.addEventListener('change', changeHandler)
+form.addEventListener('submit', submitHandler)
+
+
+let delay;
+let promiseFinalStatus
+function changeHandler(event){
+  delay = event.currentTarget.delay.value
+  promiseFinalStatus=event.currentTarget.state.value
+  console.log("Promise import:", promiseImport)
+  }
+
+
+  function createPromise( delay , promiseFinalStatus)
+ {// Create promise
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (promiseFinalStatus === 'fulfilled') {
+        resolve(`Fulfilled promise in ${delay}ms`)
+      } else {
+        reject(`Rejected promise in ${delay}ms`)
+      
+      }
+    }, delay);
+  });
+    
+    promise.then(
+    value => {
+        (iziToast.success({
+            title: 'OK',
+            message: value,
+            timeout:'5000',
+          messageColor:'#ffffff',
+          titleColor:"#fff",
+          titleSize:"16",
+          titleLineHeight:'24',
+          iconUrl: './img/bi_check2-circle.svg',
+          iconColor:'#fff',
+          backgroundColor:'#59A10D',
+          progressBarColor:"#326101",
+          position:'topRight',
+          messageSize:'16',
+          messageLineHeight:'24',
+        }));
+    
+    },
+    error => {
+          (iziToast.error({
+            timeout:'5000',
+            messageColor:'#ffffff',
+            title:'Error',
+            titleColor:"#fff",
+            titleSize:"16",
+            titleLineHeight:'24',
+            message: error,
+            iconUrl: './img/error.svg',
+            iconColor:'#fff',
+            backgroundColor:'#EF4040',
+            progressBarColor:"#B51B1B",
+            position:'topRight',
+            messageSize:'16',
+            messageLineHeight:'24',
+        }));
+    }
+  );
+}
+ 
+  function submitHandler(event){ 
+    event.preventDefault();
+    createPromise(delay , promiseFinalStatus)
+    form.reset()
+    }
+
+
